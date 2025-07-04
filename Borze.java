f125a2c112deeea7ded8782589a1078d492d4fc5
kRamu81 <kanamramu18@gmail.com>
2025-07-04 10:08:18 +0530

    add

---
+++ Borze.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Borze {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String st = s.next();
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<st.length();i++){
+            if(st.charAt(i)=='.'){
+                ans.append("0");
+            }else if (i + 1 < st.length()){
+                if (st.charAt(i + 1) == '.') {
+                    ans.append("1");
+                }else {
+                    ans.append("2");
+                }
+                i++;
+            }
+        }
+        System.out.println(ans.toString());
+    }
+}
